795d5489a7864b9671ea50b5d5f3e558514aa60c
JoeyBone99 <aparicio_joe@hotmail.com>
2022-02-19 17:12:57 -0700

    first commit

---
+++ preWork.py
@@ -0,0 +1,52 @@
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
+
+def hello_name(user_name):
+    
+    print('hello_' + user_name +'!')
+    
+hello_name('Joeyloke')
+
+
+# Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
+def first_odds():
+    range(0, 100)
+    for i in range(0, 100 + 1):
+        if(i%2!=0):
+            print(i)
+
+
+# Please write a Python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below.
+def max_num_in_list(a_list):
+    numbers= [1,2,3,4,5,6,]
+    print(numbers[5])
+
+max_num_in_list(())
+
+
+# Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false).
+'''  no clue guys! hoping you can help me sort out some things! '''
+
+        
+
+
+
+# Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
+
+def is_consecutive(n_list):
+    n_list = 1, 2, 3, 4, 5, 6
+    for element in n_list:
+        n_list[element] = int([n_list])
+    if element == range(1,7):
+           print(True)
+              
+is_consecutive(n_list)
+
+
+
+
+
+
+
+
+    
+    
