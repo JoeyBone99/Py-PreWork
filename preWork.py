841ffc70ab4b3594cc8924b2f24bf706661bfb80
JoeyBone99 <aparicio_joe@hotmail.com>
2022-03-11 12:22:19 -0700

    testing

--- preWork.py
+++ preWork.py
@@ -7,39 +7,68 @@ def hello_name(user_name):
 hello_name('Joeyloke')
 
 
-# Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
+# Write a python function, first_odds that prints the odd numbers 
+# from 1-100 and returns nothing
+
 def first_odds():
-    range(0, 100)
-    for i in range(0, 100 + 1):
-        if(i%2!=0):
-            print(i)
+   numbers = []
+   for value in range(1,100):
+      if value %2 != 0:
+         numbers.append(value)
+   return None
+   
+print(first_odds())
 
 
-# Please write a Python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below.
-def max_num_in_list(a_list):
-    numbers= [1,2,3,4,5,6,]
-    print(numbers[5])
+# Please write a Python function max_num_in_list
+# to return the max number of a given list.
+# The first line of the code has been defined as below.
 
-max_num_in_list(())
+def max_num(numbs):
+   return max(numbs)
+   
+print(max_num([2, 4, 6, 5, 3]))
 
 
-# Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false).
-'''  no clue guys! hoping you can help me sort out some things! '''
+# Write a function to return if the given year is a leap year. 
+# A leap year is divisible by 4, but not divisible by 100, 
+# unless it is also divisible by 400. 
+# The return should be boolean Type (true/false).
 
-        
+given_year = (2004, 2008, 2005, 2003)
+def leap_year(given_year):
+    for given_years in given_year:
+        if given_years % 4 == 0:
+            print(True)
+        elif given_years % 400 == 0:
+            print(True)
+        elif given_years % 100 == 0:
+            print(True)
+        else:
+            print(False)
 
+leap_year(given_year)
 
 
 # Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
 
-def is_consecutive(n_list):
-    n_list = 1, 2, 3, 4, 5, 6
-    for element in n_list:
-        n_list[element] = int([n_list])
-    if element == range(1,7):
-           print(True)
-              
-is_consecutive(n_list)
+def consecutive(numbers):
+   for i in range(len(numbers)-1):
+      if numbers[i]+1 != numbers[i+1]:
+        print(False)
+
+consecutive([5, 3, 7, 13])
+
+
+
+
+
+
+
+
+
+
+
 
 
 
